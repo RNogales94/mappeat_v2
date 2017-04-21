84b149241cc6b1d94aee8f4bccdfbf72aaf633d3
RNogales94 <rnogales@correo.ugr.es>
2017-04-21 17:27:16 +0200

    Add API documentation

--- mappeat/mappeat/mainRest/serializers.py
+++ mappeat/mappeat/mainRest/serializers.py
@@ -34,6 +34,11 @@ class MesureUnitySerializer(serializers.ModelSerializer):
         model = Mesure_Unity
         fields = ('name', 'simbol')
 
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Owner
+        fields = "__all__"         
+
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
@@ -73,14 +78,61 @@ class StaffSerializer(serializers.ModelSerializer):
         rol = Ref_Staff_Rol.objects.all().filter(staf_role_description=rol_choice)
         staff = Staff.objects.create(staff_role_code=rol[0], **validated_data)
         return staff
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = "__all__" 
         
+class Menu_CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu_Category
+        fields = "__all__" 
         
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__" 
         
+class Ticket_ResumeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket_Resume
+        fields = "__all__"         
+
+class Ticket_DetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket_Detail
+        fields = "__all__"         
+
+"""
+#######################
+"""
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"         
+            
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = "__all__"         
+            
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = "__all__"         
+            
+
 # ViewSets define the view behavior.
 class MesureUnityViewSet(viewsets.ModelViewSet):
     queryset = Mesure_Unity.objects.all()
     serializer_class = MesureUnitySerializer
 
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer    
@@ -103,21 +155,59 @@ class Ref_Staff_RolViewSet(viewsets.ModelViewSet):
     
 class StaffViewSet(viewsets.ModelViewSet):
     queryset = Staff.objects.all()
-    serializer_class = StaffSerializer 
+    serializer_class = StaffSerializer
+    
+class TableViewSet(viewsets.ModelViewSet):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer 
+    
+class Menu_CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Menu_Category.objects.all()
+    serializer_class = Menu_CategorySerializer    
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer  
+    
+class Ticket_ResumeViewSet(viewsets.ModelViewSet):
+    queryset = Ticket_Resume.objects.all()
+    serializer_class = Ticket_ResumeSerializer 
+    
+class Ticket_DetailViewSet(viewsets.ModelViewSet):
+    queryset = Ticket_Detail.objects.all()
+    serializer_class = Ticket_DetailSerializer  
+    
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer  
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer  
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Ticket_Detail.objects.all()
+    serializer_class = Ticket_DetailSerializer  
+
     
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'mesureUnities', MesureUnityViewSet)
+router.register(r'owners', OwnerViewSet)
 router.register(r'restaurants', RestaurantViewSet)
 router.register(r'providers', ProviderViewSet)
 router.register(r'suply_categories', Supply_CategoryViewSet)
 router.register(r'suplies', SupplyViewSet)
 router.register(r'ref_staff_roles', Ref_Staff_RolViewSet)
 router.register(r'staff', StaffViewSet)
-
-
-
-
+router.register(r'tables', TableViewSet)
+router.register(r'menu_categories', Menu_CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'ticket_resumes', Ticket_ResumeViewSet)
+router.register(r'ticket_details', Ticket_DetailViewSet)
+router.register(r'ingredients', IngredientViewSet)
+router.register(r'services', ServiceViewSet)
+router.register(r'inventory', InventoryViewSet)
 
 
 
--- mappeat/mappeat/mappeat/urls.py
+++ mappeat/mappeat/mappeat/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mainRest/', include('mainRest.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^dev/docs/', include_docs_urls(title='Mappeat API V1'))
 ]
