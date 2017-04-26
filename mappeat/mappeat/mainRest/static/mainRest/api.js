4fbdbd64c747f77e632f59841493bd374bce965d
RNogales94 <rnogales@correo.ugr.es>
2017-04-26 17:48:58 +0200

    Add mainRest static files configuration support

---
+++ mappeat/mappeat/mainRest/static/mainRest/api.js
@@ -0,0 +1,20 @@
+var baseURI = "http://127.0.0.1:8000/mainRest/api/v1/"
+
+function get(component, onLoad) {
+	"use strict;"
+	let request = new XMLHttpRequest();
+	request.open("GET", baseURI + component);
+	request.responseType = "json";
+	request.onload = onLoad;
+	request.send();
+}
+
+function post(component, onLoad, data) {
+	"use strict;"
+	let request = new XMLHttpRequest();
+	request.open("POST", baseURI + component);
+	request.setRequestHeader("Content-type", "application/json");
+	request.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+	request.onload = onLoad;
+	request.send(JSON.stringify(data));
+}
