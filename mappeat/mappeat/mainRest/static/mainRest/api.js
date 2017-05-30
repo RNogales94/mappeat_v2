63145a7fd580deac892a1c11285525f7b6f5729f
Luis Castro Martín <luiscastro193@gmail.com>
2017-05-30 22:19:52 +0200

    Manejo automático de errores

--- mappeat/mappeat/mainRest/static/mainRest/api.js
+++ mappeat/mappeat/mainRest/static/mainRest/api.js
@@ -1,18 +1,40 @@
-function get(component, onLoad, baseURI = "/mainRest/api/v1/") {
+function get(component, onLoad, baseURI = "/mainRest/api/v1/", handleErrors = false) {
 	"use strict;"
 	let request = new XMLHttpRequest();
 	request.open("GET", baseURI + component);
 	request.responseType = "json";
-	request.onload = function(){ onLoad.call(this) };
+	
+	if(handleErrors)
+		request.onload = onLoad;
+	else{
+		request.onload = function(){
+			if (this.status >= 400)
+				alert("Error: " + this.statusText + "\n\n" + this.responseText);
+			else
+				onLoad.call(this);
+		};
+	}
+	
 	request.send();
 }
 
-function post(component, onLoad, data, baseURI = "/mainRest/api/v1/") {
+function post(component, onLoad, data, baseURI = "/mainRest/api/v1/", handleErrors = false) {
 	"use strict;"
 	let request = new XMLHttpRequest();
 	request.open("POST", baseURI + component);
 	request.setRequestHeader("Content-type", "application/json");
 	request.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
-	request.onload = function(){ onLoad.call(this) };
+	
+	if(handleErrors)
+		request.onload = onLoad;
+	else{
+		request.onload = function(){
+			if (this.status >= 400)
+				alert("Error: " + this.statusText + "\n\n" + this.responseText);
+			else
+				onLoad.call(this);
+		};
+	}
+	
 	request.send(JSON.stringify(data));
 }
